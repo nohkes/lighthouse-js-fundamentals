52933dc99c53431b4969352127800723659b9a2e
Rawle <amandeepsekhon404@gmail.com>
2018-07-30 00:15:44 +0000

    Adds looping exercises

--- ingredients.js
+++ ingredients.js
@@ -7,10 +7,10 @@ while (i < ingredients.length) {
   i++;
 }
 // Write a for loop that prints out the contents of ingredients:
-for (var i = 0; i < ingredients.length; i++); {
-console.log(ingredients[i]);
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
 }
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
-for (var i = ingredients.length - 1; i >= 0; i--); {
+for (var i = ingredients.length - 1; i >= 0; i--) {
   console.log(ingredients[i]);
 }
